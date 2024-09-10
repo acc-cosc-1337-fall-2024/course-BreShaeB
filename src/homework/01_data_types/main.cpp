0664d25aa3b5c7f4f33422807914da99811a9c2c
BreShaeB <144928340+BreShaeB@users.noreply.github.com>
2024-09-10 00:23:44 +0000

    homework1 finished

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,23 @@
 ﻿//write include statements
-
-
+#include<iostream> //needs to be included when using cout or cin
+#include "data_types.h" //tells c++ where to find the function multiply numbers
 //write namespace using statement for cout
-
+using std::cout; using std::cin; //also needs to be included when using cout and cin
 
 int main()
 {
-	
+	int num;
+	cout<<"Enter a number: ";
+	cin>>num;
 
+	int result = multiply_numbers(num);
+
+	cout<<"Result: "<<result<<"\n";
+
+	int num1 = 4;
+
+	result = multiply_numbers(num1);
+
+	cout<<"Result: "<<result<<"\n";
 	return 0;
 }
