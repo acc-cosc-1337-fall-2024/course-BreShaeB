11a10e0ad1f17eaafdc4330507dcabd0407e07ce
BreShaeB <144928340+BreShaeB@users.noreply.github.com>
2024-09-29 08:31:19 +0000

    semi done

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,41 @@
 //write include statements
+#include<iostream>
+#include<string>
+#include "repetition.h"
 
 //write using statements
+using std::cout; using std::cin;
 
 int main() 
 {
-	return 0;
+	int user_option; int product2; int product; int num; int num1; int num2; int result;
+
+	do
+	{
+		cout<<"Main Menu \n 1- Factorial \n 2- Greatest Common Divisor \n 3- Exit \n Option: ";
+    	cin>> user_option;
+
+		switch(user_option)
+		{
+			case 1:
+			cout<<"Enter a number: ";
+			cin>> num;
+			product2 = factorial(num);
+			cout<<"Factorial: "; cout<<product2; cout<<"\n";
+			break;
+
+			case 2:
+			cout<<"Enter number 1: ";
+			cin>> num1;
+			cout<<"Enter number 2: ";
+			cin>> num2;
+			result = gcd(num1, num2);
+			cout<<"Factorial: "; cout<<result; cout<<"\n";
+			break;
+
+			case 3:
+			cout<<"You've exited the program'";
+			break;
+		}
+	} while (user_option != 3);
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,40 @@
 //add include statements
+#include "repetition.h"
+#include<string>
+using std::cout; using std::cin; using std::string;
 
 //add function(s) code here
+
+int factorial(int num)
+{
+    auto product = 0;
+    while (num > 0)
+    {
+        for (auto i = num + 1; i >= num; i++)
+        {
+            product = num * (i-1);
+        }
+    }
+    return product;
+}
+
+int gcd(int num1, int num2)
+{
+    if (num1 == 0)
+    {
+        return num2;
+    }
+    if (num2 == 0)
+    {
+        return num1;
+    }
+
+    if (num1 > num2)
+    {
+        return gcd((num1-num2), num2);
+    }
+    if (num2 > num1)
+    {
+        return gcd((num2-num1), num2);
+    }
+}
